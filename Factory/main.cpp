ef1a5bd2ff663c186e290addd470ec3f3811e4d1
alkeity <alkeity@DESKTOP-FGO9B7J>
2024-06-02 14:52:28 +0300

    add Shapes classes

--- Factory/main.cpp
+++ Factory/main.cpp
@@ -1,42 +1,39 @@
-#include<iostream>
-#include<raylib.h>
-
+﻿#include"Shapes.h"
 using std::cout;
 using std::cin;
 using std::endl;
 
-class Shape
-{
-protected:
-	//
-public:
-};
-
-class Circle :public Shape
-{
-
-};
-
-class Triangle : public Shape
-{
-
-};
-
-class Rectangle :public Shape
-{
-protected:
-	//
-public:
-};
-
-class Square : public Rectangle
-{
-
-};
+//В проекте 'Factory' реализовать иерархию геометрических фигур :
+//Квадрат, Прямоугольник, Крут, треугольник.......
+//для каждой фигуры нужно вывести ее первичные свойства, например,
+//длина сторон, радиус ........и вторичные свойства : диагональ,
+//площадь, периметр....
+//Так же каждую фигуру нужно нарисовать.
+//
+//Необходимо сгенерировать все эти фигуры в случайном порядке со случайными свойствами.
 
 
 int main()
 {
 	setlocale(LC_ALL, "");
 
+	MyShape* shapes[4]{ new MyCircle(30, VIOLET), new SquaredTriangle(40, 30, SKYBLUE), new MyRectangle(50, 30, BEIGE), new MySquare(40, PINK)};
+
+	const int screenWidth = 800;
+	const int screenHeight = 600;
+
+	InitWindow(screenWidth, screenHeight, "Shapes!");
+	SetTargetFPS(30);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		for (size_t i = 0; i < 4; i++)
+		{
+			shapes[i]->draw(Vector2(100, 100 + i * 100));
+		}
+		EndDrawing();
+	}
+	CloseWindow();
 }
